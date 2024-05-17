d03c5b13d3ab9d683ed55d9bb97ebd3e4d659c06
nipun <nipundilshan991@gmail.com>
2024-05-17 22:48:01 +0530

    newchanges

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,64 +1,49 @@
 import 'package:exam_result/util/colors.dart';
+import 'package:exam_result/util/reusable.dart';
 import 'package:exam_result/viewpage.dart';
 import 'package:flutter/material.dart';
 
-class Loginpage extends StatelessWidget {
+class Loginpage extends StatefulWidget {
   const Loginpage({super.key});
 
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _usernameTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     String username = " ";
     String password = " ";
     return Scaffold(
       body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
           hexStringToColor("CB2B93"),
           hexStringToColor("9546C4"),
           hexStringToColor("5E61F4")
         ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("Nipun Huttooo"),
-            const SizedBox(height: 20),
-            TextField(
-              onChanged: (text) {
-                username = text;
-              },
-              obscureText: false,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
-                hintText: 'Enter Username',
-              ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                reusableTextField("Enter Username", Icons.person, false,
+                    _usernameTextController),
+                const SizedBox(height: 30),
+                reusableTextField("Enter Password", Icons.lock, false,
+                    _passwordTextController),
+                const SizedBox(height: 30),
+                signInButton(context, () {})
+              ],
             ),
-            const SizedBox(height: 20),
-            TextField(
-              onChanged: (text) {
-                password = text;
-              },
-              obscureText: false,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                hintText: 'Enter Password',
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: () {
-                  print(username);
-                  print(password);
-                  if (username == "i" && password == "i") {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const viewpage()),
-                    );
-                  }
-                },
-                child: const Text("Log In")),
-          ],
+          ),
         ),
       ),
     );
--- lib/util/reusable.dart
+++ lib/util/reusable.dart
@@ -28,3 +28,31 @@ TextField reusableTextField(String text, IconData icon, bool isPasswordType,
         : TextInputType.emailAddress,
   );
 }
+
+Container signInButton(BuildContext context, Function onTap) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+    child: ElevatedButton(
+      onPressed: () {
+        onTap();
+      },
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.black26;
+            }
+            return Colors.white;
+          }),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))),
+      child: Text(
+        'Log In',
+        style: const TextStyle(
+            color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+    ),
+  );
+}
