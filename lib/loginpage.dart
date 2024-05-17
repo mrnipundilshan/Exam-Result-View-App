bb11f5fde36d0e24ea1686b3bfc0403a27442fb6
nipun <nipundilshan991@gmail.com>
2024-05-17 22:26:11 +0530

    newchanges

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:exam_result/util/colors.dart';
 import 'package:exam_result/viewpage.dart';
 import 'package:flutter/material.dart';
 
@@ -9,48 +10,56 @@ class Loginpage extends StatelessWidget {
     String username = " ";
     String password = " ";
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text("Nipun Huttooo"),
-          const SizedBox(height: 20),
-          TextField(
-            onChanged: (text) {
-              username = text;
-            },
-            obscureText: false,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Username',
-              hintText: 'Enter Username',
-            ),
-          ),
-          const SizedBox(height: 20),
-          TextField(
-            onChanged: (text) {
-              password = text;
-            },
-            obscureText: false,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-              hintText: 'Enter Password',
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-              onPressed: () {
-                print(username);
-                print(password);
-                if (username == "i" && password == "i") {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const viewpage()),
-                  );
-                }
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("CB2B93"),
+          hexStringToColor("9546C4"),
+          hexStringToColor("5E61F4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("Nipun Huttooo"),
+            const SizedBox(height: 20),
+            TextField(
+              onChanged: (text) {
+                username = text;
               },
-              child: const Text("Log In")),
-        ],
+              obscureText: false,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+                hintText: 'Enter Username',
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              onChanged: (text) {
+                password = text;
+              },
+              obscureText: false,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Enter Password',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  print(username);
+                  print(password);
+                  if (username == "i" && password == "i") {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const viewpage()),
+                    );
+                  }
+                },
+                child: const Text("Log In")),
+          ],
+        ),
       ),
     );
   }
