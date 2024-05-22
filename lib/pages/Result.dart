110454d0eb31ef571c79adf05491e0dca853ec17
nipun <nipundilshan991@gmail.com>
2024-05-22 09:20:32 +0530

    newchanges

--- lib/pages/Result.dart
+++ lib/pages/Result.dart
@@ -4,11 +4,12 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text("Moda paka",
-            style: TextStyle(fontSize: 24, color: Colors.black)),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Text("1st Year"),
+            Text("1st Semester"),
+          ],
+        ));
   }
 }
