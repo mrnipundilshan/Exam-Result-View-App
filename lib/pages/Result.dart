bb4838f75bf0582961bfc6b8ca2b5ad164d4b9b2
nipun <nipundilshan991@gmail.com>
2024-05-23 12:11:54 +0530

    newchanges

--- lib/pages/Result.dart
+++ lib/pages/Result.dart
@@ -1,67 +1,99 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Result extends StatelessWidget {
+  const Result({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Text("1st Year"),
-            Text("1st Semester"),
-            DataTable(
-              columns: [
-                DataColumn(
-                  label: Text(
-                    'Name',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Age',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Role',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-              rows: [
-                DataRow(
-                  cells: [
-                    DataCell(Text('John Doe')),
-                    DataCell(Text('25')),
-                    DataCell(Text('Developer')),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text('Jane Smith')),
-                    DataCell(Text('30')),
-                    DataCell(Text('Designer')),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text('Mike Johnson')),
-                    DataCell(Text('35')),
-                    DataCell(Text('Manager')),
-                  ],
-                ),
-              ],
-              headingRowColor:
-                  MaterialStateColor.resolveWith((states) => Colors.blue),
-              dataRowColor: MaterialStateColor.resolveWith(
-                (states) => states.contains(MaterialState.selected)
-                    ? Colors.blue.withOpacity(0.2)
-                    : Colors.blue.withOpacity(0.1),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20.0),
+          const Text(
+            "1st Year",
+            style: TextStyle(fontSize: 30),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 35,
               ),
+              Text(
+                "1st Semester",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          DataTable(
+            columns: const [
+              DataColumn(
+                label: Text(
+                  'Subject',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Theory',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Practiacal',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              DataColumn(
+                label: Text(
+                  'Overall',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+            rows: const [
+              DataRow(
+                cells: [
+                  DataCell(Center(child: Text('IT'))),
+                  DataCell(Center(child: Text('A'))),
+                  DataCell(Center(child: Text('A+'))),
+                  DataCell(Center(child: Text('A+'))),
+                ],
+              ),
+              DataRow(
+                cells: [
+                  DataCell(Center(child: Text('IT'))),
+                  DataCell(Center(child: Text('A'))),
+                  DataCell(Center(child: Text('A+'))),
+                  DataCell(Center(child: Text('A+'))),
+                ],
+              ),
+              DataRow(
+                cells: [
+                  DataCell(Center(child: Text('IT'))),
+                  DataCell(Center(child: Text('A'))),
+                  DataCell(Center(child: Text('A+'))),
+                  DataCell(Center(child: Text('A+'))),
+                ],
+              ),
+            ],
+            headingRowColor:
+                MaterialStateColor.resolveWith((states) => Colors.blue),
+            dataRowColor: MaterialStateColor.resolveWith(
+              (states) => states.contains(MaterialState.selected)
+                  ? Colors.blue.withOpacity(0.2)
+                  : Colors.blue.withOpacity(0.0),
             ),
-          ],
-        ));
+            border: TableBorder.all(
+                color: Color.fromARGB(255, 255, 255, 255), width: 1.5),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/test.dart
+++ lib/test.dart
@@ -1,129 +1,16 @@
-import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
-import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
 
-class Test extends StatefulWidget {
-  @override
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  int _page = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
-
-  final List<Widget> _pages = [
-    HomePage(),
-    SearchPage(),
-    ChatPage(),
-    FeedPage(),
-    PersonalPage(),
-  ];
-
+class test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
-        key: _bottomNavigationKey,
-        index: 0,
-        items: const [
-          CurvedNavigationBarItem(
-            child: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          CurvedNavigationBarItem(
-            child: Icon(Icons.search),
-            label: 'Search',
-          ),
-          CurvedNavigationBarItem(
-            child: Icon(Icons.chat_bubble_outline),
-            label: 'Chat',
-          ),
-          CurvedNavigationBarItem(
-            child: Icon(Icons.newspaper),
-            label: 'Feed',
-          ),
-          CurvedNavigationBarItem(
-            child: Icon(Icons.perm_identity),
-            label: 'Personal',
-          ),
-        ],
-        color: Colors.white,
-        buttonBackgroundColor: Colors.white,
-        backgroundColor: Colors.blueAccent,
-        animationCurve: Curves.easeInOut,
-        animationDuration: Duration(milliseconds: 600),
-        onTap: (index) {
-          setState(() {
-            _page = index;
-          });
-        },
-        letIndexChange: (index) => true,
-      ),
-      body: _pages[_page],
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
-      child: Center(
-        child: Text("Home Page",
-            style: TextStyle(fontSize: 24, color: Colors.white)),
-      ),
-    );
-  }
-}
-
-class SearchPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
-      child: Center(
-        child: Text("Search Page",
-            style: TextStyle(fontSize: 24, color: Colors.white)),
-      ),
-    );
-  }
-}
-
-class ChatPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orangeAccent,
-      child: Center(
-        child: Text("Chat Page",
-            style: TextStyle(fontSize: 24, color: Colors.white)),
-      ),
-    );
-  }
-}
-
-class FeedPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.redAccent,
-      child: Center(
-        child: Text("Feed Page",
-            style: TextStyle(fontSize: 24, color: Colors.white)),
-      ),
-    );
-  }
-}
-
-class PersonalPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purpleAccent,
-      child: Center(
-        child: Text("Personal Page",
-            style: TextStyle(fontSize: 24, color: Colors.white)),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Upload Excel Data to Firestore'),
+        ),
+        body: Center(
+          child: Text('Upload Excel Data'),
+        ),
       ),
     );
   }
